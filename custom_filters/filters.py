27f6fa838206fe34dc79ed24b3ae1eb2bab709b0
k1berteftel <kkulis985@gmail.com>
2025-02-26 13:05:54 +0300

    fix

--- custom_filters/filters.py
+++ custom_filters/filters.py
@@ -9,4 +9,4 @@ class StartDialogFilter(Filter):
         self.keyword = key
 
     async def __call__(self, msg: Message, translator: Translator):
-        return msg.text.strip() == translator[self.keyword].strip()
+        return msg.text and msg.text.strip() == translator[self.keyword].strip()
