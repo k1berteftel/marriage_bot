cbcdf5d314cb9cdd8635b8bdb13eb290c1accec2
k1berteftel <kkulis985@gmail.com>
2025-03-08 02:15:00 +0300

    fix

--- utils/filter_functions.py
+++ utils/filter_functions.py
@@ -3,25 +3,19 @@ from database.model import FormTable, UsersTable
 
 
 async def sort_forms(forms: list[int], session: DataInteraction) -> list[int]:
-    counter = 0
     n = len(forms)
     for i in range(n):
         for j in range(0, n-i-1):
-            first_form = await session.get_form_by_id(forms[j+1])
-            second_form = await session.get_form_by_id(forms[j])
-            if first_form.boost and not second_form.boost:
-                forms[j], forms[j+1] = forms[j+1], forms[j]
-                counter = j + 1
-
-    n = len(forms[counter::])
-    print(n)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            form = await session.get_form_by_id(forms[j])
-            first_user = await session.get_user(form.user_id)
-            form = await session.get_form_by_id(forms[j+1])
-            second_user = await session.get_user(form.user_id)
-            if not first_user.vip and second_user.vip:
+            first_form = await session.get_form_by_id(forms[j])
+            first_user = await session.get_user(first_form.user_id)
+            second_form = await session.get_form_by_id(forms[j+1])
+            second_user = await session.get_user(second_form.user_id)
+            if (
+                    (not first_form.boost and second_form.boost) or
+                    ((first_user.vip and not first_form.photos) and (second_user.vip and second_form.photos)) or
+                    (not first_form.photos and second_form.photos) or
+                    (not first_user.vip and second_user.vip)
+            ):
                 forms[j], forms[j+1] = forms[j+1], forms[j]
 
     return forms
